16658e288985428083c5f6f89d60e332a8d2d7b8
Larissa S <larissafschiavo@gmail.com>
2017-09-01 02:49:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ function total() {
 
   var ans = 0
   for(let i = 0; i < cart.length; i++){
-    let v = Object.values(cart[i])
+    let v = cart[i]
     ans = ans + v
   }
-  return ans 
+  return ans
 }
 
 function removeFromCart(item) {
