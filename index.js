3f16d6cdfd16dd62ff0fe37454f803afa0e379a5
Larissa S <larissafschiavo@gmail.com>
2017-09-01 03:39:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,5 +86,5 @@ function placeOrder(cardNumber) {
 
   let totalCartPrice = total(cart)
 
-  console.log(`Your total cost is ${totalCartPrice}, which will be charged to the card ${cardNumber}.`)
+  console.log(`Your total cost is $\${totalCartPrice}, which will be charged to the card ${cardNumber}.`)
 }
